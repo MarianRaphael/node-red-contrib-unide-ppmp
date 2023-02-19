66e2b5105926f39fe730329df07c1c62fcb16052
MarianRapahel <73583313+MarianRaphael@users.noreply.github.com>
2023-02-19 12:00:26 +0100

    1.1.2 update

--- validation/ppmp-validation.js
+++ validation/ppmp-validation.js
@@ -1,59 +1,78 @@
 module.exports = function (RED) {
-    function ppmpValidation(config) {
-      RED.nodes.createNode(this, config);
-      var node = this;
-      node.version = config.version;
-      node.type = config.type;
-      var Ajv = require("ajv");
-      var ajv = new Ajv();
-  
-      if (node.version == "3" && node.type == "process") {
-        var schema = "/schemas/v3/process_schema.json";
-      }
-      if (node.version == "3" && node.type == "measurement") {
-        var schema = "/schemas/v3/measurement_schema.json";
-      }
-      if (node.version == "3" && node.type == "message") {
-        var schema = "/schemas/v3/message_schema.json";
-      }
-      if (node.version == "2" && node.type == "process") {
-        var schema = "/schemas/v2/process_schema.json";
-      }
-      if (node.version == "2" && node.type == "measurement") {
-        var schema = "/schemas/v2/measurement_schema.json";
-      }
-      if (node.version == "2" && node.type == "message") {
-        var schema = "/schemas/v2/message_schema.json";
-      }
-  
-      node.on("input", function (msg) {
-        try {
-          var data = JSON.parse(msg.payload);
-          var validate = ajv.compile(schema);
-          var valid = validate(data);
-  
-          if (valid) {
-            node.status({ fill: "green", shape: "dot", text: "Valid JSON" });
-            node.send([{ payload: data }, null]);
-            node.debug("JSON is valid");
-          } else {
-            node.status({ fill: "red", shape: "dot", text: "Invalid JSON" });
-            var errors = validate.errors
-              .map(function (e) {
-                return e.message;
-              })
-              .join(", ");
-            node.send([null, { payload: errors }]);
-            node.error("JSON is invalid: " + errors);
-          }
-        } catch (error) {
-          node.status({ fill: "red", shape: "dot", text: "Invalid JSON" });
-          node.error("JSON parse error: " + error.message);
-          node.send([null, { payload: "JSON parse error: " + error.message }]);
-        }
-      });
+  const Ajv = require("ajv");
+  const ajv = new Ajv();
+  const fs = require("fs");
+
+  function ppmpValidation(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.version = config.version;
+    node.type = config.type;
+
+    if (node.version == "3" && node.type == "process") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v3/process_schema.json", { encoding: "utf8" })
+      );
     }
-  
-    RED.nodes.registerType("PPMP-Validation", ppmpValidation);
-  };
-  
+    if (node.version == "3" && node.type == "measurement") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v3/measurement_schema.json", {
+          encoding: "utf8",
+        })
+      );
+    }
+    if (node.version == "3" && node.type == "message") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v3/message_schema.json", { encoding: "utf8" })
+      );
+    }
+    if (node.version == "2" && node.type == "process") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v2/process_schema.json", { encoding: "utf8" })
+      );
+    }
+    if (node.version == "2" && node.type == "measurement") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v2/measurement_schema.json", {
+          encoding: "utf8",
+        })
+      );
+    }
+    if (node.version == "2" && node.type == "message") {
+      var schema = JSON.parse(
+        fs.readFileSync("/schemas/v2/message_schema.json", { encoding: "utf8" })
+      );
+    }
+
+    var validate = ajv.compile(schema);
+
+    // Handle incoming messages
+    node.on("input", function (msg) {
+      try {
+        var data = JSON.parse(msg.payload);
+        var valid = validate(data);
+
+        if (valid) {
+          node.status({ fill: "green", shape: "dot", text: "Valid PPMP" });
+          node.send([{ payload: data }, null]);
+          node.debug("PPMP is valid");
+        } else {
+          node.status({ fill: "red", shape: "dot", text: "Invalid PPMP" });
+          var errors = validate.errors
+            .map(function (e) {
+              return e.message;
+            })
+            .join(", ");
+          //node.send([null, { payload: errors }]);
+          node.error("PPMP is invalid: " + errors);
+        }
+      } catch (error) {
+        node.status({ fill: "red", shape: "dot", text: "Invalid PPMP" });
+        node.error("PPMP parse error: " + error.message);
+        //node.send([null, { payload: "JSON parse error: " + error.message }]);
+      }
+    });
+  }
+
+  RED.nodes.registerType("PPMP-Validation", ppmpValidation);
+};
